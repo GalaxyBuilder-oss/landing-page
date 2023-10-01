a3bae36c1e35946733e4ea499cb222968ab927dc
GalaxyBuilder <23426493+GalaxyBuilder-oss@users.noreply.github.com>
2023-10-01 23:48:12 +0700

    menghapus alert

--- js/main.js
+++ js/main.js
@@ -2,4 +2,4 @@ let favIcon = document.createElement("link");
 favIcon.setAttribute("rel","Shortcut Icon");
 favIcon.href="../favicon.ico";
 document.head.append(favIcon);
-alert("this code is running test");
+// alert("this code is running test");
