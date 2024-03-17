7db85e49299ae3e2779f686d385b50bec5112601
GalaxyBuilder <23426493+GalaxyBuilder-oss@users.noreply.github.com>
2024-03-17 08:00:12 +0700

    Delete js directory
    not used

--- js/main.js
+++
@@ -1,5 +0,0 @@
-let favIcon = document.createElement("link");
-favIcon.setAttribute("rel","Shortcut Icon");
-favIcon.href="../favicon.ico";
-document.head.append(favIcon);
-// alert("this code is running test");
